77c61bd13303fa3e9545dca97022c3478296b92f
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2023-08-04 13:55:25 +0300

    fix final issues and code cleanup

--- src/app.js
+++ src/app.js
@@ -2,97 +2,94 @@
  * Service for Scanning Files
  */
 
-const logger = require("../common/logger");
-const helper = require("../common/helper");
-const Kafka = require("no-kafka");
-const healthcheck = require("topcoder-healthcheck-dropin");
-const ScannerService = require("./services/ScannerService");
-const config = require("config"); 
+const logger = require('./common/logger')
+const Kafka = require('no-kafka')
+const healthcheck = require('topcoder-healthcheck-dropin')
+const ScannerService = require('./services/ScannerService')
+const config = require('config')
 
 // create consumer
 const options = {
-    connectionString: config.KAFKA_URL,
-    groupId: config.KAFKA_GROUP_ID,
-    };
-    if (config.KAFKA_CLIENT_CERT && config.KAFKA_CLIENT_CERT_KEY) {
-    options.ssl = {
-        cert: config.KAFKA_CLIENT_CERT,
-        key: config.KAFKA_CLIENT_CERT_KEY,
-    };
-    }
-    const consumer = new Kafka.GroupConsumer(options);
+  connectionString: config.KAFKA_URL,
+  groupId: config.KAFKA_GROUP_ID
+}
+if (config.KAFKA_CLIENT_CERT && config.KAFKA_CLIENT_CERT_KEY) {
+  options.ssl = {
+    cert: config.KAFKA_CLIENT_CERT,
+    key: config.KAFKA_CLIENT_CERT_KEY
+  }
+}
+const consumer = new Kafka.GroupConsumer(options)
 
-    const topics = [config.AVSCAN_TOPIC];
+const topics = [config.AVSCAN_TOPIC]
 
 // data handler
 const dataHandler = (messageSet, topic, partition) =>
   Promise.each(messageSet, async (m) => {
-    const message = m.message.value.toString("utf8");
+    const message = m.message.value.toString('utf8')
     logger.info(
       `Handle Kafka event message; Topic: ${topic}; Partition: ${partition}; Offset: ${m.offset}; Message: ${message}.`
-    );
-    let messageJSON;
+    )
+    let messageJSON
     try {
-      messageJSON = JSON.parse(message);
+      messageJSON = JSON.parse(message)
     } catch (e) {
-      logger.error("Invalid message JSON.");
-      logger.error(e);
+      logger.error('Invalid message JSON.')
+      logger.error(e)
       // ignore the message
-      return;
+      return
     }
 
     // Check if the topic in the payload is same as the Kafka topic
     if (messageJSON.topic !== topic) {
       logger.error(
         `The message topic ${messageJSON.topic} doesn't match the Kafka topic ${topic}.`
-      );
+      )
       // ignore the message
-      return;
+      return
     }
 
     try {
-      await ScannerService.processScan(messageJSON);
-      consumer.commitOffset({ topic, partition, offset: m.offset });
+      await ScannerService.processScan(messageJSON)
+      consumer.commitOffset({ topic, partition, offset: m.offset })
     } catch (err) {
-      logger.error(err);
+      logger.error(err)
 
       // commit offset regardless of errors
-      consumer.commitOffset({ topic, partition, offset: m.offset });
+      consumer.commitOffset({ topic, partition, offset: m.offset })
     }
-});
+  })
 
 /*
  * Function to check if the Kafka connection is alive
  */
-function check() {
-    if (
-      !consumer.client.initialBrokers &&
+function check () {
+  if (
+    !consumer.client.initialBrokers &&
       !consumer.client.initialBrokers.length
-    ) {
-      return false;
-    }
-    let connected = true;
-    consumer.client.initialBrokers.forEach((conn) => {
-      logger.debug(`url ${conn.server()} - connected=${conn.connected}`);
-      connected = conn.connected & connected;
-    });
-    return connected;
+  ) {
+    return false
   }
-  
-  consumer
-    .init([
-      {
-        subscriptions: topics,
-        handler: dataHandler,
-      },
-    ])
-    // consume configured topics
-    .then(() => {
-      logger.info("Initialized.......");
-      healthcheck.init([check]);
-      logger.info("Adding topics successfully.......");
-      logger.info(topics);
-      logger.info("Kick Start.......");
-    });
+  let connected = true
+  consumer.client.initialBrokers.forEach((conn) => {
+    logger.debug(`url ${conn.server()} - connected=${conn.connected}`)
+    connected = conn.connected & connected
+  })
+  return connected
+}
 
-
+consumer
+  .init([
+    {
+      subscriptions: topics,
+      handler: dataHandler
+    }
+  ])
+// consume configured topics
+  .then(() => {
+    logger.info('Initialized.......')
+    healthcheck.init([check])
+    logger.info('Adding topics successfully.......')
+    logger.info(topics)
+    logger.info('Kick Start.......')
+  })
